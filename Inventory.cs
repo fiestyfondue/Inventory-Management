1864ba53d8c6b00a0093c0d6e86eaf36b1fcfc4e
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-06-11 18:57:46 +0530

    [Harsh]Add:- Inventory class is added

---
+++ Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeW3
+{
+    public class Inventory
+    {
+        public string Name { set; get; }
+        public int weight { set; get; }
+        public float Price{ set; get; }
+
+    };
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PracticeW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory rice = new Inventory()
+            {
+                Name = "Basmati",
+                weight = 30,
+                Price = 20
+            }; 
+            Inventory pulses = new Inventory()
+            {
+                Name = "Daal",
+                weight = 40,
+                Price = 35
+            };
+            Inventory wheats = new Inventory()
+            {
+              
+                Name = "Wheat",
+                weight = 100,
+                Price = 25
+            };
+
+            List<Inventory> inventories = new List<Inventory>();
+            inventories.Add(rice);
+            inventories.Add(pulses);
+            inventories.Add(wheats);
+
+            //Serialization object from string to json format.//
+            string strResult = JsonConvert.SerializeObject(inventories);
+            File.WriteAllText(@"Inventort.json", strResult);
+            Console.WriteLine("Stored successfully!!");
+
+            //Deserialization object from json file to string format.//
+            strResult = File.ReadAllText(@"Inventort.json");
+            var list = JsonConvert.DeserializeObject<List<Inventory>>(strResult);
+            float totalValue = 0;
+            foreach (Inventory inventory in list)
+            {
+                Console.WriteLine($"Name: {inventory.Name}\n Weight: {inventory.weight}\n Price: {inventory.Price}/Kg\n Total Price: {inventory.weight * inventory.Price}\n");
+                totalValue += (inventory.Price * inventory.weight);
+            }
+            Console.WriteLine($"Total Value of inventory:{totalValue}");
+
+        }
+
+    }    
+}
